07e5c12064242ee0d694750ed7d1868ce734ccfa
黄克凡 <w545459363@126.com>
2021-01-15 15:32:02 +0800

    commit

---
+++ src/main/java/com/tilitili/common/mapper/TagMapper.java
@@ -0,0 +1,22 @@
+package com.tilitili.common.mapper;
+
+import com.tilitili.common.entity.Tag;
+import com.tilitili.common.entity.query.TagQuery;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public interface TagMapper {
+
+    List<Tag> list(TagQuery tagQuery);
+    int count(TagQuery tagQuery);
+    int insert(Tag tag);
+    int update(Tag tag);
+
+    @Select("select * from [tag] where id = #{id}")
+    @ResultMap("TagResultMap")
+    Tag getById(Long id);
+}
