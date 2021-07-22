e25b08e356f1aa7c784acb727cc1687740e7315d
黄克凡 <w545459363@126.com>
2021-07-22 17:25:37 +0800

    debug

--- src/main/java/com/tilitili/common/manager/ResourcesManager.java
+++ src/main/java/com/tilitili/common/manager/ResourcesManager.java
@@ -4,6 +4,9 @@ import com.tilitili.common.mapper.ResourcesMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+import java.util.Optional;
+
 @Component
 public class ResourcesManager {
     private final ResourcesMapper resourcesMapper;
@@ -20,6 +23,11 @@ public class ResourcesManager {
 
     public Integer getRecommendIssue() {
         return Integer.valueOf(resourcesMapper.getValueByType("RECOMMEND_ISSUE"));
+    }
+
+    public Boolean isForwardTempMessage() {
+        String str = Optional.ofNullable(resourcesMapper.getValueByType("is_forward_temp_message")).orElse("").trim();
+        return Objects.equals(str, "1");
     }
 
 }
